972ce707c0cf69147004487fc57b24d46cd1266e
kjunu <junwooo.kim@gmail.com>
2019-01-17 19:30:25 +0900

    [system contract] Remove gob and implements serialization

--- contract/system/staking.go
+++ contract/system/staking.go
@@ -6,7 +6,7 @@ package system
 
 import (
 	"bytes"
-	"encoding/gob"
+	"encoding/binary"
 	"errors"
 	"math/big"
 
@@ -74,13 +74,7 @@ func unstaking(txBody *types.TxBody, sender *state.V, scs *state.ContractState, 
 
 func setStaking(scs *state.ContractState, who []byte, staking *types.Staking) error {
 	key := append(stakingkey, who...)
-	var data bytes.Buffer
-	enc := gob.NewEncoder(&data)
-	err := enc.Encode(staking)
-	if err != nil {
-		return err
-	}
-	return scs.SetData(key, data.Bytes())
+	return scs.SetData(key, serializeStaking(staking))
 }
 
 func getStaking(scs *state.ContractState, who []byte) (*types.Staking, error) {
@@ -91,11 +85,7 @@ func getStaking(scs *state.ContractState, who []byte) (*types.Staking, error) {
 	}
 	var staking types.Staking
 	if len(data) != 0 {
-		dec := gob.NewDecoder(bytes.NewBuffer(data))
-		err = dec.Decode(&staking)
-		if err != nil {
-			return nil, err
-		}
+		return deserializeStaking(data), nil
 	}
 	return &staking, nil
 }
@@ -105,4 +95,25 @@ func GetStaking(scs *state.ContractState, address []byte) (*types.Staking, error
 		return getStaking(scs, address)
 	}
 	return nil, errors.New("invalid argument: address should not be nil")
+}
+
+func serializeStaking(v *types.Staking) []byte {
+	var ret []byte
+	if v != nil {
+		ret = append(ret, v.GetAmount()...)
+		ret = append(ret, vsep)
+		when := make([]byte, 8)
+		binary.LittleEndian.PutUint64(when, v.GetWhen())
+		ret = append(ret, when...)
+	}
+	return ret
+}
+
+func deserializeStaking(data []byte) *types.Staking {
+	datas := bytes.Split(data, []byte{vsep})
+	if len(datas[1]) != 8 {
+		panic("staking data corruption")
+	}
+	when := binary.LittleEndian.Uint64(datas[1])
+	return &types.Staking{Amount: datas[0], When: when}
 }
