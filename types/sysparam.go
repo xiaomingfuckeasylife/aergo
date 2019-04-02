7a7ea6120f6368234285ad9d7c19ad416fd40793
Sung-Jae Woo <s.covariant@gmail.com>
2019-04-02 18:25:05 +0900

    [sysparam] reimplement SpInt64 to embed SysParam (protobuf)

--- types/sysparam.go
+++ types/sysparam.go
@@ -2,27 +2,9 @@
  *  @file sysparam.go
  *  @copyright defined in aergo/LICENSE.txt
  */
-package config
+package types
 
 import "strconv"
-
-// SysParam is a type for system parameter.
-type SysParam struct {
-	name  string
-	desc  string
-	mut   bool
-	value SpValue
-}
-
-// NewSysParam returns a new SysParam.
-func NewSysParam(name string, desc string, mut bool, spv SpValue) *SysParam {
-	return &SysParam{
-		name:  name,
-		desc:  desc,
-		mut:   mut,
-		value: spv,
-	}
-}
 
 // SpValue is an interface, which is an abstraction of system parameter values.
 type SpValue interface {
@@ -39,19 +21,55 @@ type SpValue interface {
 
 // SpInt64 implements SpValue where the parameter value is a 64 bit signed
 // interger (int64).
-type SpInt64 int64
+type SpInt64 struct {
+	*SysParam
+	i64Value int64
+	cached   bool
+}
+
+// NewSpInt64 returns a new SpInt64.
+func NewSpInt64(name, desc, val string, mut, dao bool) *SpInt64 {
+	v := &SpInt64{
+		SysParam: &SysParam{
+			Name: name,
+			Desc: desc,
+			Mut:  mut,
+			Dao:  dao,
+		},
+	}
+	if err := v.SetString(val); err != nil {
+		return nil
+	}
+
+	return v
+}
 
 // SetString sets i to the integer converted from v.
 func (i *SpInt64) SetString(v string) error {
-	x, err := strconv.ParseInt(v, 10, 64)
-	*i = SpInt64(x)
+	var err error
+
+	if i.i64Value, err = strconv.ParseInt(v, 10, 64); err == nil {
+		i.Value = v
+		i.cached = true
+	}
 
 	return err
 }
 
 // Get returns *i.
 func (i *SpInt64) Get() interface{} {
-	return *i
+	if i.cached {
+		return i.i64Value
+	}
+
+	var err error
+	if i.i64Value, err = strconv.ParseInt(i.GetValue(), 10, 64); err != nil {
+		return nil
+	}
+
+	i.cached = true
+
+	return i.i64Value
 }
 
 // SetBytes sets *i to b.
@@ -61,7 +79,7 @@ func (i *SpInt64) SetBytes(b []byte) error {
 
 // GetBytes returns the marshaled byte array of *i.
 func (i *SpInt64) GetBytes() []byte {
-	return []byte(strconv.FormatInt(int64(*i), 64))
+	return []byte(i.GetValue())
 }
 
 // SpStr implements SpValue where the parameter value is a string.
